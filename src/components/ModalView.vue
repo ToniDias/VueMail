8b9d1cd193190b7a8cf581b06b6316c9d7fd15eb
ToniDias <toni.dias.91@gmail.com>
2020-11-05 21:11:11 +0100

    add keydown escape to close ModalView

--- src/components/ModalView.vue
+++ src/components/ModalView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="modal">
-    <div class="overlay" @click="emit('closeModal')"></div>
+    <div class="overlay" @click="emit('close-modal')"></div>
     <div class="modal-card">
       <slot />
     </div>
@@ -8,13 +8,23 @@
 </template>
 
 <script>
+import { onBeforeUnmount } from 'vue'
 export default {
   setup(props, { emit }) {
+    let onKeydown = (event) => {
+      if (event.key === 'Escape') {
+        emit('close-modal')
+      }
+    }
+    window.addEventListener('keydown', onKeydown)
+    onBeforeUnmount(() => {
+      window.removeEventListener('close-modal')
+    })
     return {
       emit,
-    };
+    }
   },
-};
+}
 </script>
 
 <style scoped></style>

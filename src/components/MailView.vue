61728cad383db7d1f19952921280cc52b3c12541
ToniDias <toni.dias.91@gmail.com>
2020-11-06 08:10:30 +0100

    add change-email on MailView event to handle all actions

--- src/components/MailView.vue
+++ src/components/MailView.vue
@@ -26,32 +26,60 @@
 <script>
 import { format } from 'date-fns'
 import marked from 'marked'
-import axios from 'axios'
-import { localhost } from '@/utils.js'
+// import axios from 'axios'
+// import { localhost } from '@/utils.js'
 import useKeyDown from '@/composables/use-keydown'
 
 export default {
-  setup(props) {
+  setup(props, { emit }) {
     // eslint-disable-next-line vue/no-setup-props-destructure
-    let email = props.email
+    // const email = props.email
+    // let toggleRead = () => {
+    //   email.read = !email.read
+    //   axios.put(localhost.emails + `/${email.id}`, email)
+    // }
+    // let toggleArchive = () => {
+    //   email.archived = !email.archived
+    //   axios.put(localhost.emails + `/${email.id}`, email)
+    // }
     let toggleRead = () => {
-      email.read = !email.read
-      axios.put(localhost.emails + `/${email.id}`, email)
+      emit('change-email', { toggleRead: true, save: true })
     }
     let toggleArchive = () => {
-      email.archived = !email.archived
-      axios.put(localhost.emails + `/${email.id}`, email)
+      emit('change-email', {
+        toggleArchive: true,
+        save: true,
+        closeModal: true,
+      })
+    }
+    let goNewer = () => {
+      emit('change-email', { changeIndex: -1 })
+    }
+    let goOlder = () => {
+      emit('change-email', { changeIndex: 1 })
+    }
+    let goNewerAndArchive = () => {
+      emit('change-email', { changeIndex: -1, toggleArchive: true, save: true })
+    }
+    let goOlderAndArchive = () => {
+      emit('change-email', { changeIndex: 1, toggleArchive: true, save: true })
     }
 
     useKeyDown([
       { key: 'r', fn: toggleRead },
       { key: 'e', fn: toggleArchive },
+      { key: 'k', fn: goNewer },
+      { key: 'j', fn: goOlder },
+      { key: '(', fn: goNewerAndArchive },
+      { key: ')', fn: goOlderAndArchive },
     ])
     return {
       format,
       marked,
       toggleRead,
       toggleArchive,
+      goNewer,
+      goOlder,
     }
   },
   props: {
